d4d3609bd20cfb57de3794595ddf5f78f5cc2cc8
rtd1250 <47224354+rtd1250@users.noreply.github.com>
2021-03-11 18:53:54 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt install --reinstall git dkms build-essential linux-headers-$(uname -r)
+sudo apt install bc module-assistant
+git clone https://github.com/tomaspinho/rtl8821ce
+cd rtl8821ce
+chmod +x dkms-install.sh
+chmod +x dkms-remove.sh
+sudo ./dkms-install.sh
